d22d6c33a09eff8a631a9a4c0c1cdd4feb8598b8
osipik.a <osipik.av@yandex.by>
2022-09-06 00:01:47 +0300

    feat: add timer

--- js/script.js
+++ js/script.js
@@ -32,4 +32,28 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     }
   });
+
+  const deadline = "2022-09-07";
+  // const deadline1 = new Date(Date.now() + (60 * 1000 + 999));
+  let timerId = null;
+  const hoursSpan = document.querySelector(".hours");
+  const minutesSpan = document.querySelector(".minutes");
+  const secondsSpan = document.querySelector(".seconds");
+
+  function countdownTimer() {
+    const diff = Date.parse(deadline) - Date.parse(new Date());
+    if (diff <= 0) {
+      clearInterval(timerId);
+    }
+
+    const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+    const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+    const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+
+    hoursSpan.textContent = hours < 10 ? "0" + hours : hours;
+    minutesSpan.textContent = minutes < 10 ? "0" + minutes : minutes;
+    secondsSpan.textContent = seconds < 10 ? "0" + seconds : seconds;
+  }
+  countdownTimer();
+  timerId = setInterval(countdownTimer, 1000);
 });
